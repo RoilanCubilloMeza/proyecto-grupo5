14357b03f2c3afd81cff5fe4779abf3a69f728b2
Caro20 <carolina.segura.cordero@est.una.ac.cr>
2023-05-09 18:24:58 -0600

    Modificaciones al archivo Formulario.tsx
    Se hacen las modificaciones correspondientes a dicho archivo para los atributos que necesita.

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3a5afd5078a806ed"; }
+/******/ 		__webpack_require__.h = function() { return "9487627a74825563"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/components/Formulario.tsx
+++ src/components/Formulario.tsx
@@ -3,6 +3,9 @@ import { useState } from 'react';
 const RegistrationForm = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
+  const [company, setCompany] = useState('');
+  const [telephone, setTelephone] = useState('');
+  const [reason, setReason] = useState('');
 
   const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setName(event.target.value);
@@ -12,10 +15,25 @@ const RegistrationForm = () => {
     setEmail(event.target.value);
   };
 
+  const handleCompanyChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setCompany(event.target.value);
+  };
+
+  const handleTelephoneChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTelephone(event.target.value);
+  };
+
+  const handleReasonChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setReason(event.target.value);
+  };
+
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     console.log('Nombre:', name);
     console.log('Correo electrónico:', email);
+    console.log('Empresa:', company);
+    console.log('Telefono:', telephone);
+    console.log('Motivo:', reason);
   };
 
   return (
@@ -27,6 +45,18 @@ const RegistrationForm = () => {
       <div>
         <label htmlFor="email">Correo electrónico:</label>
         <input type="email" id="email" value={email} onChange={handleEmailChange} />
+      </div>
+      <div>
+        <label htmlFor="company">Empresa:</label>
+        <input type="text" id="company" value={company} onChange={handleCompanyChange} />
+      </div>
+      <div>
+        <label htmlFor="telephone">Telefono:</label>
+        <input type="text" id="telephone" value={telephone} onChange={handleTelephoneChange} />
+      </div>
+      <div>
+        <label htmlFor="reason">Motivo:</label>
+        <input type="text" id="reason" value={reason} onChange={handleReasonChange} />
       </div>
       <button type="submit">Enviar</button>
     </form>
