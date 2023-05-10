378a88710f6e65f89014268bd4290683edb75656
RoilanCubillo <andreycubillo69@gmail.com>
2023-05-10 14:02:08 -0600

    cambios

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1ab5cd032d1f3c9c"; }
+/******/ 		__webpack_require__.h = function() { return "fc84f7a03f345dc3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
