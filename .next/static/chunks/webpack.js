580e03fc03ec53cb59dfa76c34afbfa2da685b0a
RoilanCubillo <andreycubillo69@gmail.com>
2023-05-11 20:12:33 -0600

    creacion  de carpetas

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "fc84f7a03f345dc3"; }
+/******/ 		__webpack_require__.h = function() { return "8965def5c888342b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
